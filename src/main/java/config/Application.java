3a7e5333904ce86acf1fbd99a5d17d62b8342c60
DESKTOP-JI1N0GH\Renato <renatommps@gmail.com>
2018-08-16 16:11:11 -0300

    separada classe que inicia a aplicação e definido o caminho dos basePackages

--- src/main/java/config/Application.java
+++ src/main/java/config/Application.java
@@ -1,9 +1,11 @@
-package hello;
+package config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "rest", "service", "model" })
 public class Application {
 
     public static void main(String[] args) {
