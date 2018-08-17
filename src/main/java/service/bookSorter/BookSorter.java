37f12d658ef653be06910ab7330726c79d20340c
DESKTOP-JI1N0GH\Renato <renatommps@gmail.com>
2018-08-17 13:29:25 -0300

    Atualizado serviço de ordenação para tratar exceção. Adicionada implementação real de ordenação

--- src/main/java/service/bookSorter/BookSorter.java
+++ src/main/java/service/bookSorter/BookSorter.java
@@ -3,9 +3,11 @@ package service.bookSorter;
 import java.util.List;
 
 import model.Book;
+import util.exception.EmptyRulesException;
+import util.exception.OrderingException;
 
 
 public interface BookSorter  {
 
-    public List<Book> sort(List<Book> books);
+    public List<Book> sort(List<Book> books) throws OrderingException, EmptyRulesException;
 }
--- src/main/java/service/bookSorter/BookSorterImpl.java
+++ src/main/java/service/bookSorter/BookSorterImpl.java
@@ -1,20 +1,38 @@
 package service.bookSorter;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import model.Book;
-
+import service.bookSorter.helper.BookComparator;
+import service.bookSorter.helper.BookSortingRules;
+import service.fileReader.FileReader;
+import service.fileReader.FileReaderImpl;
+import util.exception.EmptyRulesException;
+import util.exception.OrderingException;
 
 @Service("bookSorter")
 public class BookSorterImpl implements BookSorter  {
 
     @Override
-    public List<Book> sort(List<Book> books){
+    public List<Book> sort(List<Book> books) throws OrderingException, EmptyRulesException {
+
+        FileReader fileReader = new FileReaderImpl();
         List<Book> sortedBooks = books.subList(0, books.size());
-        Collections.reverse(sortedBooks);
+        
+        String rules = fileReader.readRulesConfigFile();
+        if(rules.isEmpty()){
+            throw new EmptyRulesException();
+        }
+
+        BookSortingRules sortingRules = new BookSortingRules(rules);
+        BookComparator bookComparator = new BookComparator(sortingRules);
+
+        Collections.sort(sortedBooks, bookComparator);
+
         return sortedBooks;
     }
 }
